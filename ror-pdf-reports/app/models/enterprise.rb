7ea553f6d801c21fcefea25360bf69301de0d920
fry <jmorrispratt@gmail.com>
2014-09-17 16:16:11 -0400

    added the 'has_many' and 'belongs_to' to the corresponding models
    this aspects must be analyzed:
    . i didn't follow exactly the rails naming convention
    . i will try with this convention and see what happens

--- ror-pdf-reports/app/models/enterprise.rb
+++ ror-pdf-reports/app/models/enterprise.rb
@@ -1,4 +1,12 @@
 # generated with the following rails command:
 # rails generate model enterprise enterprise_ticker:string{50} enterprise_name:string{100}
 class Enterprise < ActiveRecord::Base
+  # has_many :to, :class_name => 'InfoselStockAction', :foreign_key => 'stock_owner_id'
+
+  # has_many :to, :class_name => 'YahooStockAction', :foreign_key => 'stock_owner_id'
+
+  # has_many :yah, :foreign_key => 'user_id', :class_name => "Task"
+  has_many :yahoo_stock_actions, :foreign_key => 'stock_owner_id', :class_name => 'YahooStockAction'
+
+  has_many :infosel_stock_actions, :foreign_key => 'stock_owner_id', :class_name => 'InfoselStockAction'
 end
